6abd942c22b208d42aac9edf832353ab990eb494
Annie Liao <liaowow@yahoo.com>
2019-11-14 16:33:29 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -14,8 +14,9 @@ def print_first_directors_movie_titles
   #grab the array that stores first director's movies
   movies_arr = directors_database[0][:movies]
   #loop thru the first key of each element in the array
-  movies_index = 0
-  while movies_index < movies_arr.count do
-    puts movies_arr[0]
+  movies_arr_index = 0
+  while movies_arr_index < movies_arr.count do
+    puts movies_arr[movies_arr_index][:title]
+    movies_arr_index += 1
   end
 end
